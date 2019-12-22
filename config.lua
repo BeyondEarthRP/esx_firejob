baa9105b79e4b3195f472a235c88c2ceae561ff2
BeyondEarthRP <BeyondRageRP@gmail.com>
2019-12-22 22:46:30 +0000

    edited the config

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@ Config.DrawDistance               = 100.0
 Config.MarkerType                 = 1
 Config.MarkerSize                 = { x = 1.5, y = 1.5, z = 1.0 }
 Config.MarkerColor                = { r = 50, g = 50, b = 204 }
-Config.EnablePlayerManagement     = true
+Config.EnablePlayerManagement     = false
 Config.EnableArmoryManagement     = false
 Config.EnableESXIdentity          = true -- only turn this on if you are using esx_identity
 Config.EnableNonFreemodePeds      = true -- turn this on if you want custom peds
